f0078761c5dc85b8010ecb26210723cf3ab711ac
ysz951 <shengyang_zhou@baylor.edu>
2020-07-16 04:52:34 -0500

    add delete and post

--- src/app.js
+++ src/app.js
@@ -4,7 +4,21 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-
+const { v4: uuid } = require('uuid');
+const users = [{
+    "id": "3c8da4d5-1597-46e7-baa1-e402aed70d80",
+    "username": "sallyStudent",
+    "password": "c00d1ng1sc00l",
+    "favoriteClub": "Cache Valley Stone Society",
+    "newsLetter": "true"
+  },
+  {
+    "id": "ce20079c-2326-4f17-8ac4-f617bfd28b7f",
+    "username": "johnBlocton",
+    "password": "veryg00dpassw0rd",
+    "favoriteClub": "Salt City Curling Club",
+    "newsLetter": "false"
+  }];
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -12,12 +26,9 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
+app.use(express.json())
 app.use(helmet())
 app.use(cors())
-
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
 
 app.use(function errorHandler(error, req, res, next) {
     let response
@@ -29,5 +40,99 @@ app.use(function errorHandler(error, req, res, next) {
     }
     res.status(500).json(response)
 })
+
+app.get('/', (req, res) => {
+    res
+      .send('A GET Request');
+});
+
+app.post('/', (req, res) => {
+    res
+      .send('POST request received.');
+});
+
+app.get('/user/:userId', (req, res) => {
+    let response;
+    const {userId} = req.params;
+    response = users.filter(user => user.id === userId);
+    res.json(response)
+})
+
+app.post('/user', (req, res) => {
+    // get the data
+    console.log(req.body)
+    const { username, password, favoriteClub, newsLetter=false } = req.body;
+    
+    // All are required, check if they were sent
+    if (!username) {
+      return res
+        .status(400)
+        .send('Username required');
+    }
+  
+    if (!password) {
+      return res
+        .status(400)
+        .send('Password required');
+    }
+  
+    if (!favoriteClub) {
+      return res
+        .status(400)
+        .send('favorite Club required');
+    }
+  
+    // make sure username is correctly formatted.
+    if (username.length < 6 || username.length > 20) {
+      return res
+        .status(400)
+        .send('Username must be between 6 and 20 characters');
+    }
+  
+    // password length
+    if (password.length < 8 || password.length > 36) {
+      return res
+        .status(400)
+        .send('Password must be between 8 and 36 characters');
+    }
+  
+    // password contains digit, using a regex here
+    if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+      return res
+        .status(400)
+        .send('Password must be contain at least one digit');
+    }
+  
+    const clubs = [
+      'Cache Valley Stone Society',
+      'Ogden Curling Club',
+      'Park City Curling Club',
+      'Salt City Curling Club',
+      'Utah Olympic Oval Curling Club'
+    ];
+  
+    // make sure the club is valid
+    if (!clubs.includes(favoriteClub)) {
+      return res
+        .status(400)
+        .send('Not a valid club');
+    }
+    const id = uuid();
+    const newUser = {
+      id,
+      username,
+      password,
+      favoriteClub,
+      newsLetter
+    };
+  
+    users.push(newUser);
+  
+    res
+    .status(201)
+    .location(`http://localhost:8000/user/${id}`)
+    .json({id: id});
+
+});
 
 module.exports = app
